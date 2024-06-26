ec39fd00cf1f5df2e19966fbdedcaa0d1c1f4f81
A-sabri <96385848+A-sabri@users.noreply.github.com>
2024-06-26 23:22:49 +0200

    modif db
    test des controlleur et modif de la bd pour enregistrer les donné dans la bonne collection

--- Back-end/config/db.js
+++ Back-end/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb+srv://" + process.env.DB_USER_PASS + "@cluster.cxuxlbv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster/code-academy-db")
+mongoose.connect("mongodb+srv://" + process.env.DB_USER_PASS + "@cluster.cxuxlbv.mongodb.net/code-academy-db?retryWrites=true&w=majority&appName=Cluster")
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch(() => console.log('Connexion à MongoDB échouée !'));
+
