5c526234a30c35d1b78cd482754ea27e55ac367a
A-sabri <96385848+A-sabri@users.noreply.github.com>
2024-06-27 01:08:35 +0200

    user routes
    finalisation des routes user ajout des middleware sur les routes

--- Back-end/routes/user.routes.js
+++ Back-end/routes/user.routes.js
@@ -1,11 +1,25 @@
+//importaion des dépendances
 const express = require('express');
 const router = express.Router();
-const userCtrl = require('../controllers/user.controller.js');
 
-router.get('/', userCtrl.getAllUsers);
-router.get('/:id', userCtrl.getUserById);
-router.post('/', userCtrl.createUser);
-router.put('/:id', userCtrl.updateUser);
-router.delete('/:id', userCtrl.deleteUser);
+//importaion des middleware
+const auth = require('../middleware/auth.middlware.js');
+const multer = require('../middleware/multer-config.user.js');
+
+//importaion des controller 
+const userCtrl = require('../controllers/user.controller.js');
+const authCtrl = require('../controllers/auth.controller.js');
+
+//auth
+router.post('/register', authCtrl.register);
+router.post('/login', authCtrl.login);
+router.get('/logout', authCtrl.logout );
+
+//user display
+router.get('/', auth, userCtrl.getAllUsers);
+router.get('/:id', auth, userCtrl.getUserById);
+router.post('/', auth, userCtrl.createUser);
+router.put('/:id', auth, multer, userCtrl.updateUser);
+router.delete('/:id', auth, userCtrl.deleteUser);
 
 module.exports = router;
