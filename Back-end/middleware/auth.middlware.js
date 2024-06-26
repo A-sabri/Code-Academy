d132716a453ef5a923eb080e0e917a7afaae0da0
A-sabri <96385848+A-sabri@users.noreply.github.com>
2024-06-27 00:28:21 +0200

    authentification
    creation de middlware et controller pour l'authentification et rajout d'un token dans une variable d'environement

---
+++ Back-end/middleware/auth.middlware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET'); 
+    const { id } = decodedToken;
+    req.auth = {
+      id,
+    };
+    next();
+  } catch {
+    res.status(401).json({ message: `Echec de l'authentification` });
+  }
+};
+
+module.exports.checkUser = (req, res, next) => {
+  
+  
+};
